df17eaddb85b1ce38852edc92a9619be6d782376
punit-j <47202792+punit-j@users.noreply.github.com>
2019-11-22 12:52:11 +0530

    Add files via upload

--- questions/Even_Odd_Number.java
+++ questions/Even_Odd_Number.java
@@ -9,11 +9,19 @@ public class Even_Odd_Number {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number upto which odd even numbers are to be printed");
+		System.out.println("Enter the number upto which odd or even numbers are to be printed");
 		int n = sc.nextInt();
+		System.out.println("Enter 'O' for odd and 'E' for even");
+		String s = sc.next();
 		List<Integer> odd_even = new ArrayList<>();
-		IntStream.range(1, n).filter(i -> (i%2 == 0)&&(i%4 != 0))
+		if(s.equalsIgnoreCase("e")){
+		IntStream.range(1, n).filter(i -> i%2 == 0)
 				.forEach(odd_even::add);
+		}
+		else if(s.equalsIgnoreCase("o")){
+			IntStream.range(1, n).filter(i -> i%2 != 0)
+			.forEach(odd_even::add);	
+		}
 		odd_even.forEach(System.out::println);
 		
 	}
