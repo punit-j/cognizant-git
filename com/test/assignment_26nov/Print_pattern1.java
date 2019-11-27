3fecfba879c7890d066402d603b65832f0b990a2
punit-j <47202792+punit-j@users.noreply.github.com>
2019-11-27 12:42:07 +0530

    Add files via upload

--- com/test/assignment_26nov/Print_pattern1.java
+++ com/test/assignment_26nov/Print_pattern1.java
@@ -5,14 +5,20 @@ public class Print_pattern1 {
 	public static void main(String[] args) {
 		int temp = 5;
 		for(int i = 1; i <= temp; i++){	//outer loop for number of lines having pattern in ascending order to be printed
+			for(int j = temp-i; j>1; j--){	//inner loop to insert the required spaces
+				System.out.print(" ");
+			}
 			for(int y = 1; y<=i; y++){	//inner loop to print the asterix in the ascending order
-				System.out.print("*");	// prints asterix
+				System.out.print("* ");	// prints asterix
 			}
 			System.out.println();	//inserts new line
 		}
 	      for (int i = temp-1; i >= 1; i--){	//outer loop for number of lines having pattern in descending order to be printed
-	           for (int j = 1; j <= i; j++){	//inner loop to print the asterix in the descending order
-	                System.out.print("*");	//prints asterix
+	    	  for(int j = temp-i; j>1; j--){	//inner loop to insert the required spaces
+					System.out.print(" ");
+				} 
+	    	  for (int j = 1; j <= i; j++){	//inner loop to print the asterix in the descending order
+	                System.out.print("* ");	//prints asterix
 	            }	
 	         System.out.println();	//inserts new line
 	        }
